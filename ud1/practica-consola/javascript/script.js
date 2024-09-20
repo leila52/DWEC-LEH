ee343350c40a2491e078fd94a9e777b4e4631a60
leila52 <lailahaddad385@gmail.com>
2024-09-20 12:26:53 +0200

    segundo comite de clase 20/9/24

--- ud1/practica-consola/javascript/script.js
+++ ud1/practica-consola/javascript/script.js
@@ -16,3 +16,13 @@ let empleados = [
 ]
 
 console
+
+let edades= [20,45,67,34]
+
+console.group("hola a todo el mundo");
+console.log("pepe");
+console.log("lucas");
+console.log("meri");
+console.log("topi");
+console.groupCollapsed("mundo");
+console.log("topi");

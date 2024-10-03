d300c663cbb13a067734d892b3624df0fa468f59
leila52 <lailahaddad385@gmail.com>
2024-10-03 20:47:03 +0200

    error pequeñp de cuando pulsa otro numero

--- ud2/parctica2/javascript/piedraPapelTijera.js
+++ ud2/parctica2/javascript/piedraPapelTijera.js
@@ -24,11 +24,47 @@ while(jugando=== true){
         console.log("adiossss pescado");
         break;
     }
-    if(eleccionPersona <1 || eleccionPersona >3){
-        console.log("error,introduce de nuevo ");
+    if(eleccionPersona >=1 || eleccionPersona <=3){
+        let eleccionMaquina=eleccionMaquinaFun();
+        if(eleccionPersona=== 1){
+            console.log("eleccion tuya: piedra")
+        }
+        if(eleccionPersona=== 2){
+            console.log("eleccion tuya: papel")
+        }
+        if(eleccionPersona=== 3){
+            console.log("eleccion tuya: tijera")
+        }
+        if(eleccionMaquina=== 1){
+            console.log("eleccion de maquina: piedra")
+        }
+        if(eleccionMaquina=== 2){
+            console.log("eleccion de maquina: papel")
+        }
+        if(eleccionMaquina=== 3){
+            console.log("eleccion de maquina: tijera")
+        }
 
+        //empate 
+        if(eleccionMaquina===eleccionPersona){
+            console.log("habeis empatado noooo");
+            empate++;
+        }
+        if((eleccionPersona===1 && eleccionMaquina===3) ||(eleccionPersona===2 && eleccionMaquina===1)|| (eleccionPersona===3 && eleccionMaquina===2)){
+            console.log("has ganado");
+            ganar++;
+        }else{
+            console.log("has perdido joooo");
+            perder++;
+        }
+
+        console.log(`marcador son: has ganado ${ganar}, has perdido  ${perder}  y habeis empatado ${empate}`)
+
+       
+
+    }else{
+        console.log("error,introduce de nuevo ");
     }
 
-    let eleccionMaquina=eleccionMaquinaFun();
-     console.log(`eleccion de jugado ${eleccionPersona}`)
+    
 }

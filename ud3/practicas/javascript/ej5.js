e72b425f32fcb3f8590cbcdeddad177730608a78
leila52 <lailahaddad385@gmail.com>
2024-10-06 18:23:38 +0200

    dddd

--- ud3/practicas/javascript/ej5.js
+++ ud3/practicas/javascript/ej5.js
@@ -9,12 +9,13 @@ console.log(socios.join(", "));
 
 console.log(`numero de socios  ${socios.length}`);
 
-//mostrar los socios albaceticamente
-let sociosEnOrden = socios.sort();
-console.log("los nombre ordenados: ");
-console.log(sociosEnOrden.join(", "));
-
 //mostrarlo en orden inverso
-let sociosInversa= socios.reverse();
 console.log("los nombre invertidos: ");
-console.log(sociosInversa.join(", "));
+console.log(socios.reverse().join(", "));
+
+//mostrar los socios albaceticamente
+
+console.log("los nombre ordenados: ");
+console.log(socios.sort().join(", "));
+
+

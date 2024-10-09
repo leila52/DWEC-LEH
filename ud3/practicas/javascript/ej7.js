555eb42de5a970c26b7b28d9ceb68c1ab3fd5060
leila52 <lailahaddad385@gmail.com>
2024-10-09 18:37:10 +0200

    mmfdm

--- ud3/practicas/javascript/ej7.js
+++ ud3/practicas/javascript/ej7.js
@@ -7,7 +7,7 @@ for(let i=0;i<2;i++){
     let nombre =prompt("introduce el nombre :");
     nombres.push(nombre);
     let edad =prompt("introduce la edad:");
-    edadess.push(edad);
+    edadess.push(parseInt(edad));
     edadTotal= edadTotal + parseInt(edad);
 }
 for(let i=0;i<2;i++){
@@ -16,9 +16,21 @@ for(let i=0;i<2;i++){
 media=parseInt(edadTotal/edadess.length);
 console.log(`la media de las edades son: ${media}`);
  
-let contenido =prompt("borrar  :");
-//reduce
+let opccionBorrarContenido=prompt("¿Deseas borrar un socio por nomnombre o contenido");
 
-edadess.find((elemento)=>elemento === contenido);
+if(opccionBorrarContenido ==='nombre'){
+    let nombreABorrar=prompt("introduce el nombre del socio que quieres borrar");
+    let indice=nombres.indexOf(nombreABorrar);
+    if(indice !== -1){
+        nombres.splice(indice,1);
+        edadess.slice(indice,1);
+        console.log("se ha borrado");
+        for(let i=0;i<2;i++){
+            console.log(`el nombres : ${nombres[i]} y su edad : ${edadess[i]}`)
+        }
+    }else{
+        console.log("error");
+    }
+}
 
 

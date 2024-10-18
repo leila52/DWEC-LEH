770d88e6fba6157696a80990843a9bf7b5abf0ed
leila52 <lailahaddad385@gmail.com>
2024-10-18 12:31:36 +0200

    ncgfvghf

--- ud4DOM/clase/javascript/ej1.js
+++ ud4DOM/clase/javascript/ej1.js
@@ -1,30 +1,45 @@
 const parrafo1={
     titulo: "primer bloque de parafos",
-    hijos:[ "este es el primer parrafo","este el segundo parafo","este el tercero"]
+    hijos:[ "este es el primer parrafo ","este el segundo parafo ","este el tercero"]
 }
 const parrafo2={
     titulo: "segundo bloque de parafos",
-    hijos:[ "este es el primer parrafo","este el segundo parafo","este el tercero"]
+    hijos:[ "este es el primer parrafo ","este el segundo parafo ","este el tercero"]
 }
 
 
 
-let h1=document.createElement("h1");
-h1.innerHTML=parrafo1.titulo;
-document.body.appendChild(h1);
+
+crearElemento("h1",parrafo1.titulo,document.body)
+
 for(let i=0;i<3;i++){
-    let p=document.createElement("p");
-    p.innerHTML=parrafo1.hijos[i];
-    document.body.appendChild(p);
+    crearElemento("p",parrafo1.hijos[i],document.body)
 }
 
 
+/*
 let h2=document.createElement("h1");
 h2.innerHTML=parrafo2.titulo;
 document.body.appendChild(h2);
+*/
+
+crearElemento("h1",parrafo2.titulo,document.body)
+
 for(let i=0;i<3;i++){
-    let pe=document.createElement("p");
-    pe.innerHTML=parrafo2.hijos[i];
-    document.body.appendChild(pe);
+    crearElemento("p",parrafo2.hijos[i],document.body)
 }
 
+
+function crearElemento(tipo,contenido,padre){
+    let hijo=document.createElement(tipo);
+    hijo.innerHTML=contenido
+    padre.appendChild(hijo);
+    /*
+    hijo.addEventListener("click",function(){
+        console.log(this.innerHTML);
+    })
+       */ 
+    hijo.addEventListener("click",function(){
+        this.remove();
+    })
+    }
---
+++ ud4DOM/clase/javascript/ej2.js
@@ -0,0 +1,13 @@
+
+
+insertar.addEventListener("click",function(){
+    let aleatorio=Math.floor(Math.random() *256) ;
+    let lista=document.createElement("li");
+    lista.innerHTML=`nuevo elemento  ${aleatorio}`;
+    document.body.insertBefore(lista,insertar);
+
+    lista.addEventListener("click",function(){
+        this.remove();
+    })
+})
+

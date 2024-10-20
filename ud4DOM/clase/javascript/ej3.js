85f0d2efb8427b970fd974fbfbb60a82ec1d29b0
leila52 <lailahaddad385@gmail.com>
2024-10-20 17:43:35 +0200

    memme

---
+++ ud4DOM/clase/javascript/ej3.js
@@ -0,0 +1,9 @@
+let input=document.getElementById('numeroCheckbox');
+let generar=document.getElementById('generar');
+let campo=document.getElementById('divCheckbox');
+
+console.log(input);
+generar.addEventListener('click',function(){
+    
+
+})

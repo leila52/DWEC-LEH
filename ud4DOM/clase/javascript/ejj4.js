e84c6c507846f9f3869c0f1db22a04a87f7a7228
leila52 <lailahaddad385@gmail.com>
2024-10-22 17:07:01 +0200

    ufffff

--- ud4DOM/clase/javascript/ejj4.js
+++ ud4DOM/clase/javascript/ejj4.js
@@ -4,13 +4,43 @@ let pelicula=document.getElementById('pelicula');
 let divInfo=document.getElementById('divInformacion');
 
 let generar=document.getElementById('generar');
-let campo=document.getElementById('divCheckbox');
 
-
+//labels
+let labelDeporte=document.querySelector('label[for="deporte"]');
+let labelSerie=document.querySelector('label[for="serie"]');
+let labelPelicula=document.querySelector('label[for="pelicula"]');
 
 generar.addEventListener('click',function(){
-    let infoDeporte=document.createElement("p");
-    infoDeporte.innerHTML=`el deporte favorito es: ${deporte.value}`;
-    divInfo.appendChild(infoDeporte);
-    infoDeporte.setAttribute('class','pet');
+
+    //cambiamos color añadiendo class
+    labelSerie.classList.add('color');
+    labelPelicula.classList.add('color');
+
+    //para comprobar si esta vacio y salga en rojo
+    if(deporte.value===""){
+        labelDeporte.style.color = 'red';
+    }else{
+        labelDeporte.classList.add('color');
+        let infoDeporte=document.createElement("p");
+        infoDeporte.innerHTML=` ${deporte.value}`;
+        infoDeporte.classList.add('resultado');
+        labelDeporte.appendChild(infoDeporte)
+        
+    }
+    //borrar input
+    deporte.remove();
+
+
+    let infoSerie=document.createElement("p");
+    infoSerie.innerHTML=` ${serie.value}`;
+    infoSerie.classList.add('resultado');
+    labelSerie.appendChild(infoSerie);
+    serie.remove();
+
+    let infoPeli=document.createElement("p");
+    infoPeli.innerHTML=`${pelicula.value}`;
+    infoPeli.classList.add('resultado');
+    divInfo.appendChild(infoPeli);
+    pelicula.remove();
+    
 })

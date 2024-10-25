45cf8afe0e001a897506e41bc3b8a8ca2679672c
leila52 <lailahaddad385@gmail.com>
2024-10-25 12:31:28 +0200

    NMASSAMD

--- ud4DOM/clase/javascript/ej5.js
+++ ud4DOM/clase/javascript/ej5.js
@@ -1,30 +1,57 @@
-function crearElemento(tipo, contenido) {
+function crearElemento(tipo, contenido,padre) {
     // Crear el elemento del tipo especificado
     let hijo = document.createElement(tipo)
     // Indicamos el contenido
     hijo.innerHTML = contenido
-    return hijo
+     //añadir el nodo al documento
+     padre.appendChild(hijo)
+     hijo.addEventListener("click", function () {
+        this.remove()
+    })
+     return hijo
 }
-function enOrden(nuevoElemento,compra){
-    let elementos=compra.getElementsByTagName("li");
-    for(let i=0;i<elementos.length;i++){
-        if(nuevoElemento.innerHTML.toLowerCase() < elementos[i].innerHTML.toLowerCase() ){
-            compra.insertBefore(nuevoElemento,elementos[i]);
-            return;
-        }
-        
+
+function enOrden(compra){
+    let conjunto=compra.querySelectorAll("li");
+    let valorArray=[];
+    conjunto.forEach((li) => valorArray.push(li.innerHTML));
+    valorArray.sort();
+    compra.innerHTML="";
+    valorArray.forEach((e) => crearElemento("li",e,compra));
+}
+//comprobar pcuando no introduce nada que no muestra Nan Y SI TIENE TAMAÑO 2
+function precioTotal(compra){
+    let total=0;
+    let conjunto=compra.querySelectorAll("li");
+    let precioArray=[];
+    conjunto.forEach((li) =>{
+        let partes=li.innerHTML.split(",");
+        let precio=parseFloat(partes[1]);
+        total=suma(precio,total);
+    });
+    crearElemento("p",`Precio total: ${total}`,compra)
+}
+
+function suma(precio,total){
+    if(total===0){
+        total=precio;
+    }else{
+        total=total+precio;
     }
-    //si no encontramos 
-    compra.appendChild(nuevoElemento);
+    return total;
 }
+
 document.getElementById("insertar").addEventListener("click",function(){
     let input=document.getElementById("producto");
     let compra=document.getElementById("compra");
     //si no esta vacio
     if(input.value!==""){
-        let elemento=crearElemento("li",input.value);
-        enOrden(elemento,compra);
+        let elemento=crearElemento("li",input.value,compra);
         input.value="";
+        enOrden(compra);
+        precioTotal(compra);
+        
     }
-
+    
 })
+//introduzca el precio y en otro campo muestre el precio total

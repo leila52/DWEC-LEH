21dea4186c63cba5f509347fa70d27a87fb62bc8
leila52 <lailahaddad385@gmail.com>
2024-11-28 12:44:34 +0100

    ghdfchgftdh

---
+++ ud5/proyectos/carritoCompra/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Menu from './componentes/menu/ Menu.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Menu />
+    </>
+  )
+}
+
+export default App
---
+++ ud5/proyectos/carritoCompra/src/componentes/menu/ Menu.jsx
@@ -0,0 +1,23 @@
+import './Menu.css'
+
+function Menu() {
+
+  return (
+   <>
+    <header>
+        <img src="https://i.pinimg.com/736x/af/e7/19/afe719ec610b907536e4ec62976697a2.jpg" alt="Logotipo" />
+        <h1>Carrito de la Compra</h1>
+        <nav>
+        <a href="#inicio">Inicio</a>
+        <a href="#nosotros">Productos</a>
+        <a href="#servicios">Carrito</a>
+        <a href="#contacto">Finalizar Compra</a>
+      </nav>
+        
+      
+    </header>
+   </>
+  )
+}
+
+export default Menu

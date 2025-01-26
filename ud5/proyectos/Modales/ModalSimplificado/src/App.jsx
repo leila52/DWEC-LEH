c10f64558e087c3fcbcc42d9c8c952566b1c9802
leila52 <lailahaddad385@gmail.com>
2025-01-26 16:08:26 +0100

    sss

---
+++ ud5/proyectos/Modales/ModalSimplificado/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Informe from './componentes/ListadoPrincipal'
+import ListadoPrincipal from './componentes/ListadoPrincipal'
+
+function App() {
+  
+
+  return (
+    <>
+      <ListadoPrincipal/>
+    </>
+  )
+}
+
+export default App

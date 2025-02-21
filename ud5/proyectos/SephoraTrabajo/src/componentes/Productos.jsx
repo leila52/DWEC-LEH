5123579480a7c6d9023c95735bed1ab124e83c26
leila52 <lailahaddad385@gmail.com>
2025-02-21 09:33:49 +0100

    yyyy

--- ud5/proyectos/SephoraTrabajo/src/componentes/Productos.jsx
+++ ud5/proyectos/SephoraTrabajo/src/componentes/Productos.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, Outlet } from 'react-router-dom';
-import "../estilos/Productos.css";
+//import "../estilos/Productos.css";
 
 const Productos = ({ }) => {
 
